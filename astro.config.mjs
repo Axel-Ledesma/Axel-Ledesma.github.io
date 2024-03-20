6da1a91e5d014d7a7ba3e7f2480db7e59198d5b1
Axel Ledesma <ledesmaaxelf@gmail.com>
2024-03-20 16:28:43 -0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://developer.dev.ar',
+  site: 'https://axel-ledesma.github.io',
 });
